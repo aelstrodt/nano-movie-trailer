de32e8ce3cbb1becc2f9e4c5918528df9157f66f
Alex <alexanderelstrodt@gmail.com>
2017-11-18 17:42:26 -0800

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 15 19:19:45 2017
-
-"""
-#import Movies class
+# import Movies class
 from media import Movies
 
-#create movie list with 5 Movies' instances
+# create movie list with 5 Movies' instances
 movie_list = [
-            Movies('The Shawshank Redemption','https://www.youtube.com/watch?v=6hB3S9bIaco','https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg'),
-            Movies('The Godfather','https://www.youtube.com/watch?v=sY1S34973zA','https://images-na.ssl-images-amazon.com/images/M/MV5BZTRmNjQ1ZDYtNDgzMy00OGE0LWE4N2YtNTkzNWQ5ZDhlNGJmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,704,1000_AL_.jpg'),
-            Movies('The Godfather: Part II','https://www.youtube.com/watch?v=qJr92K_hKl0','https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR3,0,182,268_AL_.jpg'),
-            Movies('The Dark Knight','https://www.youtube.com/watch?v=EXeTwQWrcwY','https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg'),
-            Movies('12 Angry Men','https://www.youtube.com/watch?v=A7CBKT0PWFA','https://images-na.ssl-images-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg'),
-            Movies('Fight Club','https://www.youtube.com/watch?v=SUXWAEX2jlg','https://images-na.ssl-images-amazon.com/images/M/MV5BZGY5Y2RjMmItNDg5Yy00NjUwLThjMTEtNDc2OGUzNTBiYmM1XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY256_CR0,0,172,256_AL_.jpg')
+        Movies(
+                'The Shawshank Redemption',
+                'https://www.youtube.com/watch?v=6hB3S9bIaco',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg'
+                ),
+        Movies(
+                'The Godfather',
+                'https://www.youtube.com/watch?v=sY1S34973zA',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BZTRmNjQ1ZDYtNDgzMy00OGE0LWE4N2YtNTkzNWQ5ZDhlNGJmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,704,1000_AL_.jpg'
+                ),
+        Movies(
+                'The Godfather: Part II',
+                'https://www.youtube.com/watch?v=qJr92K_hKl0',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR3,0,182,268_AL_.jpg'
+                ),
+        Movies(
+                'The Dark Knight',
+                'https://www.youtube.com/watch?v=EXeTwQWrcwY',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+                ),
+        Movies(
+                '12 Angry Men',
+                'https://www.youtube.com/watch?v=A7CBKT0PWFA',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg'
+                ),
+        Movies(
+                'Fight Club',
+                'https://www.youtube.com/watch?v=SUXWAEX2jlg',
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BZGY5Y2RjMmItNDg5Yy00NjUwLThjMTEtNDc2OGUzNTBiYmM1XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY256_CR0,0,172,256_AL_.jpg'
+                )
         ]
-
